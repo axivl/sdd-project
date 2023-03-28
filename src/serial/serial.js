e387f73e174a86d4db8a1ce2feb16f31d1e6caf9
Jack Speerin <jpspeerin@gmail.com>
2023-03-29 10:55:40 +1100

    A

--- src/serial/serial.js
+++ src/serial/serial.js
@@ -3,7 +3,6 @@
 const { SerialPort } = require("serialport");
 const Readline = require("@serialport/parser-readline");
 const port = new SerialPort({path: "/dev/ttyACM0", baudRate: 9600 });
-const parser = port.pipe(new Readline({ delimiter: "\n" }));
 // Read the port data
 port.on("open", () => {
   console.log("serial port open");
