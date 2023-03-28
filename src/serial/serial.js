3c3a73251b5c08f3482841134096b04b4c6c30c3
Jack Speerin <jpspeerin@gmail.com>
2023-03-29 10:53:39 +1100

    Yo

--- src/serial/serial.js
+++ src/serial/serial.js
@@ -1,17 +1,17 @@
 // Serial port code sourced from https://medium.com/@machadogj/arduino-and-node-js-via-serial-port-bcf9691fab6a
 
-const SerialPort = require("serialport");
-    const Readline = require("@serialport/parser-readline");
-    const port = new SerialPort("/dev/ttyACM0", { baudRate: 9600 });
-    const parser = port.pipe(new Readline({ delimiter: "\n" }));
-    // Read the port data
-    port.on("open", () => {
+const { SerialPort } = require("serialport");
+const Readline = require("@serialport/parser-readline");
+const port = new SerialPort("/dev/ttyACM0", { baudRate: 9600 });
+const parser = port.pipe(new Readline({ delimiter: "\n" }));
+// Read the port data
+port.on("open", () => {
   console.log("serial port open");
 });
 
 module.exports = {
   update(data) {
-    port.write(data)
-    port.write("😊")
+    port.write(data);
+    port.write("😊");
   },
 };
