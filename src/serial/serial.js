74f7677a3078a0481fd0285b9b69d72759133ebf
Jack Speerin <jpspeerin@gmail.com>
2023-03-30 10:29:29 +1100

    AIUWHDi

--- src/serial/serial.js
+++ src/serial/serial.js
@@ -1,7 +1,6 @@
 // Serial port code sourced from https://medium.com/@machadogj/arduino-and-node-js-via-serial-port-bcf9691fab6a
 
 const { SerialPort } = require("serialport");
-const Readline = require("@serialport/parser-readline");
 let path = ''
 let ArduinoPort = ''
 
@@ -16,7 +15,7 @@ SerialPort.list().then(ports => {
 
     if (typeof pm !== 'undefined' && pm.includes('arduino')) {
       path = port.path
-      ArduinoPort = new SerialPort(path, { baudRate: 9600 })
+      ArduinoPort = new SerialPort({path: path, baudRate: 9600 })
       ArduinoPort.on('open', function(){
         console.log(`connected! arduino is now connected at port ${path}`)
       })
